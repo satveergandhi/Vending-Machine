9a648c48d3e9d33a2dda6065b75a0dee1172267e
satveergandhi <137652664+satveergandhi@users.noreply.github.com>
2023-08-11 10:26:35 +0530

    Update VendingItemRepository.java

--- Vending-Machine/src/main/java/com/inn/vending/repository/VendingItemRepository.java
+++ Vending-Machine/src/main/java/com/inn/vending/repository/VendingItemRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.inn.vending.model.VendingItem;
 
 public interface VendingItemRepository extends JpaRepository<VendingItem, Integer> {
-	@Query("SELECT vi FROM VendingItem vi WHERE vi.row = :row")
-    List<VendingItem> findByRowNumber(int rowNumber);
+	List<VendingItem> findByRow(int row);
 }
 
